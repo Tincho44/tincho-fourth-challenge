5e39a4cc97c151f29238d4ba3a9e25ee39098fd0
Tincho44 <martinharbuet@gmail.com>
2023-03-04 02:35:12 -0300

    feat: Upload Migrations for models
    Upload basic migrations for cities, airlines and flights

--- app/Models/City.php
+++ app/Models/City.php
@@ -9,3 +9,8 @@ class City extends Model
 {
     use HasFactory;
 }
+
+// Ciudades has many fligts y airlines | no hay belongs
+// Vuelos belongs to airlines y tiene 2 belongs to -> una ciudad de origen y otra de destino , tengo id de donde llega a donde sale
+// origen y destino ids en las columnas de flights
+// Airline tiene hasmany flights, pertenecen a ciudades
